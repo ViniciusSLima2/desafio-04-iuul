bd90818c9c64e1e45feee7d4d7735f9cee2bf313
Vinicius dos Santos Lima <vinicius-lima2002@hotmail.com>
2024-12-20 10:32:54 -0300

    implementacao current url no header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,6 @@
-import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule, Location, NgOptimizedImage } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,8 +10,22 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  @ViewChild('links') linksElement!: ElementRef;
   isEnabled: boolean = false;
   changeView(){
     this.isEnabled = !this.isEnabled;
+  }
+
+  constructor(private location: Location){
+    console.log(this.location.path())
+  }
+
+  ngAfterViewInit(){
+    const allLinks = this.linksElement.nativeElement.childNodes;
+    const currentUrl = this.location.path().replace("/", "");
+    allLinks.forEach((element:any) => {
+      if(element.id === currentUrl) element.classList.add("current-url")
+      console.log(element.id)
+    });
   }
 }
